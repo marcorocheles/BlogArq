82b3471875b1006757b1d710775f894376656f27
Marco Rocheles <marcorocheles@hotmail.com>
2022-01-28 20:21:07 -0300

    Blog Funcional, todos os CRUDs funcionando

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,5 +1,5 @@
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -15,12 +15,30 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token),
+  };
+
+  refreshToken(){
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token),
+    };
+  }
+
   entrar(userLogin: UserLogin): Observable<UserLogin>{
     return this.http.post<UserLogin>('https://blogpessoalthiago.herokuapp.com/usuarios/logar', userLogin)
   }
   
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('https://blogpessoalthiago.herokuapp.com/usuarios/cadastrar', user)
+    return this.http.post<User>('https://blogpessoalthiago.herokuapp.com/usuarios/cadastrar', user, this.token)
+  }
+
+  atualizar(user: User): Observable<User>{
+    return this.http.put<User>('https://blogpessoalthiago.herokuapp.com/usuarios/atualizar', user, this.token)
+  }
+
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://blogpessoalthiago.herokuapp.com/usuarios/${id}`, this.token)
   }
 
   logado(){
